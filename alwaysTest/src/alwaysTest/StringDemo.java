a4442abfc087fb34fae6b2c46f2b882eea974e94
AAASus <1803559906@qq.com>
2019-04-27 17:25:24 +0800

    （4）正则表达式

--- alwaysTest/src/alwaysTest/StringDemo.java
+++ alwaysTest/src/alwaysTest/StringDemo.java
@@ -1,5 +1,8 @@
 package alwaysTest;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringDemo {
 
 	public static void main(String[] args) {
@@ -17,6 +20,18 @@ public class StringDemo {
 		System.out.println(s.substring(10,14));   //(2) 取出子字符串"test"  //substring
 	 
 		System.out.println(s.toCharArray());     //(3) 将本字符串复制到一个字符数组Char[] str中.// toCharArray
-   }
+		
+		StringBuffer buf = new StringBuffer();
+ 
+        Pattern p = Pattern.compile("\\b\\w");
+        Matcher m = p.matcher(s);
+         
+        while (m.find()) {
+            m.appendReplacement(buf, m.group().toUpperCase());
+        }
+        m.appendTail(buf);
+        System.out.println(buf);
+		
+	}
 }
 
