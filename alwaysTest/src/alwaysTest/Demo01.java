e924513247bfdf278f0b25ddf432212a5d0dbf75
AAASus <1803559906@qq.com>
2019-04-28 23:28:24 +0800

    （1）string对象与stringBuffer对象的相互转换

---
+++ alwaysTest/src/alwaysTest/Demo01.java
@@ -0,0 +1,14 @@
+package alwaysTest;
+
+public class Demo01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "this is a test of java";
+		char[] a = {1,2,3};
+		StringBuffer sb = new StringBuffer(s);   //string对象与stringBuffer对象的相互转换
+		
+
+	}
+
+}
