ad8f2ad737b16ab7156381d116a43c4a354c8c91
AAASus <1803559906@qq.com>
2019-04-30 22:11:57 +0800

    （3）将字符串中连续出现的字符压缩成一个。比如aaabbbccccd，压缩后变成abcd。

--- alwaysTest/src/alwaysTest/Demo03.java
+++ alwaysTest/src/alwaysTest/Demo03.java
@@ -7,7 +7,8 @@ public class Demo03 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String str1="woaijava183.114.221.78wuwuwuwuwuwu11122";
+		
+		String str1="woaijava183.114.221.78wuwuwuwuwuwu11122";   //（2）将一个字符串中所有的ip地址打印出来
 		String re="((2[0-4]\\d|25[0-5]|[01]?\\d\\d?)\\.){3}(2[0-4]\\d|25[0-5]|[01]?\\d\\d?)";
 		Pattern p = Pattern.compile(re);
 		Matcher m = p.matcher(str1);
@@ -15,6 +16,11 @@ public class Demo03 {
 			String result=m.group();
 			System.out.println(result);
 			}
+		
+		String str2="aaabbbccccd";                   //（3）将字符串中连续出现的字符压缩成一个。比如aaabbbccccd，压缩后变成abcd。
+		String rs=str2.replaceAll("(.)(\\1)*", "$1");
+		System.out.println(rs);
+
 
 
 
