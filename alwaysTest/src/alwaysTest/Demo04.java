6fdd3d1d3b8908099de9f12240df054db061d586
AAASus <1803559906@qq.com>
2019-05-01 16:34:23 +0800

    first commit

---
+++ alwaysTest/src/alwaysTest/Demo04.java
@@ -0,0 +1,19 @@
+package alwaysTest;
+
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Scanner;
+
+public class Demo04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("日期格式为(yyyy-MM-dd（年月日）)请输入:");
+		String s = sc.nextLine();
+		System.out.println(s);
+
+	}
+
+}
