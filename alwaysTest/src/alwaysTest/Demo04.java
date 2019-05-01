9fcce3b80679efb55b604efa4b55044a0987bede
AAASus <1803559906@qq.com>
2019-05-01 18:05:30 +0800

    修改当前系统时间（年份、小时）

--- alwaysTest/src/alwaysTest/Demo04.java
+++ alwaysTest/src/alwaysTest/Demo04.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
+import java.time.temporal.ChronoUnit;
 import java.util.Scanner;
 
 public class Demo04 {
@@ -21,12 +22,22 @@ public class Demo04 {
 		cd = cd.plusDays(7);
 		System.out.println("当前日期的后七天为："+cd);
 		
+		LocalDateTime cdt = LocalDateTime.now();
 		System.out.println();
 		System.out.println("————————————————————————使用java8的日期时间包获取当前系统时间，并以各种格式加以显示————————————————————————");
-        LocalDateTime cdt = LocalDateTime.now();
+        
 		System.out.println("当前SHORT型日期时间为："+cdt.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT,FormatStyle.SHORT)));
 		System.out.println("当前MEDIUM型日期时间为："+cdt.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM,FormatStyle.MEDIUM)));
 		System.out.println("当前LONG型日期时间为："+cdt.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.LONG,FormatStyle.LONG)));
+		
+		System.out.println();
+		System.out.println("———————————使用java8的日期时间包获取当前系统时间，并修改这个时间（分别修改年份、月份、天、小时），然后打印结果———————————");
+		
+		LocalDateTime cdt1=cdt.plus(2,ChronoUnit.YEARS);        //修改年份
+		System.out.println("两年后的时间为："+cdt1.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.LONG)));
+		
+		LocalDateTime cdt4=cdt.plusHours(5);                    //修改小时
+		System.out.println("五小时候的时间为："+cdt4.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.LONG,FormatStyle.LONG)));
 	}
 
 }
