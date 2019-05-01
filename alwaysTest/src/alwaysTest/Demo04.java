31ee635c2f9252a1547cbd0aacac2d366258ca3a
AAASus <1803559906@qq.com>
2019-05-01 17:29:15 +0800

    Second commit

--- alwaysTest/src/alwaysTest/Demo04.java
+++ alwaysTest/src/alwaysTest/Demo04.java
@@ -1,5 +1,7 @@
 package alwaysTest;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.util.Scanner;
@@ -9,10 +11,14 @@ public class Demo04 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("日期格式为(yyyy-MM-dd（年月日）)请输入:");
+		DateTimeFormatter formatter1 = DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("日期格式为(yyyy-MM-dd（年月日）)请输入:");
 		String s = sc.nextLine();
-		System.out.println(s);
+		LocalDate cd = LocalDate.parse(s, formatter1);
+		
+		cd = cd.plusDays(7);
+		System.out.println("当前日期的后七天为："+cd);
 
 	}
 
