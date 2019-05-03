8db995c5f8ded9ed3e66dbf8695e2544c3ee02f0
AAASus <1803559906@qq.com>
2019-05-03 10:58:52 +0800

    second commit:实现包装类与基本数据类型之间转换

--- alwaysTest/src/alwaysTest/Demo02.java
+++ alwaysTest/src/alwaysTest/Demo02.java
@@ -4,9 +4,18 @@ public class Demo02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String s = "123";                   //实现包装类与基本数据类型之间转换
-		int a = Integer.parseInt(s);
-		System.out.println(a);
+		String s1 = "123";                   //实现包装类与基本数据类型之间转换
+		int a1 = Integer.parseInt(s1);
+		System.out.println(a1);
+		int a2 = new Integer(s1);
+		System.out.println(a2);
+		
+		String s2 = "3.14159" ;
+		float b1 = Float.parseFloat(s2);
+		System.out.println(b1);
+		
+		
+		
 
 	}
 
