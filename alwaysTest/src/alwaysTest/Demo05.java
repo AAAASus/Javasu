8fda383cbee9aed78884d2f8125b55ca2825ba26
AAASus <1803559906@qq.com>
2019-05-04 12:26:51 +0800

    first commit：随机生成十个数

---
+++ alwaysTest/src/alwaysTest/Demo05.java
@@ -0,0 +1,16 @@
+package alwaysTest;
+
+import java.util.Random;
+
+public class Demo05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Random r = new Random();
+		for(int i = 0;i<10;i++) {
+			System.out.println(r.nextInt(100));
+		}
+
+	}
+
+}
