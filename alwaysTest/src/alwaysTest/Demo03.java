083222929b2bfafddafda5c3563ea600da93b1c1
AAASus <1803559906@qq.com>
2019-05-04 11:12:50 +0800

    （8）将一个字符串中的所有整数提取出来，保存在一维数组中。比如”ae256dd—w348e6”，提取出256,348,6三个整数

--- alwaysTest/src/alwaysTest/Demo03.java
+++ alwaysTest/src/alwaysTest/Demo03.java
@@ -20,10 +20,21 @@ public class Demo03 {
 		String str2="aaabbbccccd";                   //（3）将字符串中连续出现的字符压缩成一个。比如aaabbbccccd，压缩后变成abcd。
 		String rs=str2.replaceAll("(.)(\\1)*", "$1");
 		System.out.println(rs);
-
-
-
-
+		
+		int arr[] = new int[10];    //（8）将一个字符串中的所有整数提取出来，保存在一维数组中。比如”ae256dd—w348e6”，提取出256,348,6三个整数
+		String str3 = "ae256dd—w348e6";
+		Pattern p1 = Pattern.compile("(\\d{1,3})");
+		Matcher m1 = p1.matcher(str3);
+		int i = 0;
+		while (m1.find()) {
+			int j = 0;
+			j=Integer.parseInt(m1.group());
+			arr[i]=j;
+			i++;
+		}
+		for(int c=0;c<i;c++) {
+			System.out.print(" "+arr[c]);
+		}
 	}
 
 }
