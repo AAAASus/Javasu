ffb7d9870b709a2bed7e3963ceb1ed64844b267b
AAASus <1803559906@qq.com>
2019-05-05 09:45:50 +0800

    随机产生10个整数并保存到数组中

--- alwaysTest/src/alwaysTest/Demo05.java
+++ alwaysTest/src/alwaysTest/Demo05.java
@@ -7,9 +7,13 @@ public class Demo05 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Random r = new Random();
-		for(int i = 0;i<10;i++) {
-			System.out.println(r.nextInt(100));
-		}
+		int arr[] = new int[10];
+	        for(int i=0;i<arr.length;i++){
+	            int temp =  (int)(Math.random()*10)+1;  //随机产生10 个整数
+	            arr[i] = temp;                         //将产生的数保存到数组
+	            System.out.print(arr[i]+" ");
+	        }
+	        System.out.println();
 
 	}
 
