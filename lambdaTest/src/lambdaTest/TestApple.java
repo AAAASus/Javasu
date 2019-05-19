2890d884d3f2610c7eee514c16f248d30e954ef4
AAASus <1803559906@qq.com>
2019-05-19 16:45:31 +0800

    创建苹果对象并放到苹果数组里

--- lambdaTest/src/lambdaTest/TestApple.java
+++ lambdaTest/src/lambdaTest/TestApple.java
@@ -24,6 +24,13 @@ public class TestApple {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//创建苹果对象并放到苹果数组里
+		Apple a1 = new Apple("red",5.00);     
+		Apple a2 = new Apple("green",10.00);
+		Apple a3 = new Apple("blue",8.00);
+		Apple a4 = new Apple("yellow",9.00);
+		Apple[] a = new Apple[4]; 
+		
 
 	}
 
