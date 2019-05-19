5104b143ced691df1a48e73e45abd258071af388
AAASus <1803559906@qq.com>
2019-05-19 15:18:28 +0800

    1

---
+++ lambdaTest/src/lambdaTest/TestApple.java
@@ -0,0 +1,19 @@
+package lambdaTest;
+
+class Apple{
+	String color;
+	double Weight;
+	public Apple(String color,double weight){
+		this.color = color;
+		this.Weight = weight;
+	}
+}
+
+public class TestApple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
