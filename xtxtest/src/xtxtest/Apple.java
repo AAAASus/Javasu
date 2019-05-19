e0b63eddd8e767a9e686b2a3c1d93871b7500bf9
AAASus <1803559906@qq.com>
2019-05-19 09:57:37 +0800

    first commit

---
+++ xtxtest/src/xtxtest/Apple.java
@@ -0,0 +1,17 @@
+package xtxtest;
+
+public class Apple{
+
+		// TODO Auto-generated method stub
+		private int weight = 0;
+		private String color = "";
+		
+        public Apple(int weight, String color){
+		this.weight = weight;
+		this.color = color;
+		     }
+		
+
+	
+
+}
