6945f401d5dc16a43ac77775befba8d6fae4bdae
AAASus <1803559906@qq.com>
2019-05-19 11:39:32 +0800

    定义一个苹果类，有颜色和重量。

---
+++ xtxTest/src/xtxTest/appleTest.java
@@ -0,0 +1,20 @@
+package xtxTest;
+
+class Apple{
+	String color;
+	double weight;
+	
+	public Apple(String color,double weight){
+		this.color = color;
+		this.weight = weight;
+	}
+}
+
+public class appleTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
