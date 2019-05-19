bb6cd57cbeee240ca0e5778ca3cb7ff3f781b623
AAASus <1803559906@qq.com>
2019-05-19 11:16:07 +0800

    定义一个苹果类

---
+++ xtxtest/src/xtxtest/appleTest.java
@@ -0,0 +1,19 @@
+package xtxtest;
+
+class Apple{
+	String color;
+	double weight;
+	public Apple(String color,double weight) {
+		this.color = color;
+		this.weight = weight;
+	}
+}
+
+public class appleTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
