b2c233fd596a4b0e9b99256bbac38c435071424c
AAASus <1803559906@qq.com>
2019-05-23 16:53:36 +0800

    按测评总分排序。如果分数相同，按姓名排序

--- rfTest/src/student.java
+++ rfTest/src/student.java
@@ -2,7 +2,7 @@ import java.util.Iterator;
 import java.util.Set;
 import java.util.TreeSet;
 
-public class student {
+class student implements Comparable {
 	private int id;
 	private String name;
 	private double grade;
@@ -15,16 +15,27 @@ public class student {
 		return "id="+id+"-"+"name="+name+"-"+"grade="+grade;
 	}
 	
-
+@Override
+	public int compareTo(Object o) {
+		// TODO Auto-generated method stub
+		student s = (student) o;
+		int  result = grade>s.grade?1:(grade==s.grade?0:-1);
+		if(0==result) {
+			result=name.compareTo(s.name);
+		}
+		return result;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Set<student> set = new TreeSet<student>();
 		set.add(new student(01,"°¢Üç",80.5));
 		set.add(new student(02,"°¢Çï",82.0));
 		set.add(new student(03,"°¢æÃ",83.5));
-		set.add(new student(03,"°¢æÃ",78.0));
+		set.add(new student(03,"°¢æÃ",90.0));
 		Iterator<student> it = set.iterator();
+		
 
 	}
+	
 
 }
