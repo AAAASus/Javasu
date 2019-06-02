17437716dd99bc0d8d369f80b8ff5234072394f7
AAASus <1803559906@qq.com>
2019-06-02 11:20:13 +0800

    利用keyset方法遍历map

--- rfTest/src/Student01.java
+++ rfTest/src/Student01.java
@@ -20,7 +20,6 @@ public class Student01 implements Comparable<Student01>{
 	@Override
 	public int compareTo(Student01 o) {
 		// TODO Auto-generated method stub
-		
 			   Student01 stu = (Student01) o;
 				int  result = compare(this.name,stu.name)?1:(age>stu.age?0:-1);
 				if(0==result) {
@@ -36,6 +35,21 @@ public class Student01 implements Comparable<Student01>{
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Student01 s1 = new Student01("张三", 15);
+		Student01 s2 = new Student01("李四", 16);
+		Student01 s3 = new Student01("王五", 16);
+		HashMap<Student01, String> hm = new HashMap<Student01, String>();
+		hm.put(s1, "北京");
+		hm.put(s2,"重庆");
+		hm.put(s3,"上海");
+		System.out.println("利用keyset方式 遍历MAP");
+		Set<Student01> keys = hm.keySet();
+		Iterator<Student01> it1 = keys.iterator();
+		  while(it1.hasNext()) {
+			  Student01 s = it1.next();
+			  String values1 = hm.get(s);
+			  System.out.println(s+"  "+values1);
+		  }
 		
 	}
 
