01b47f56a6b286131536e0124156398512e646d0
AAASus <1803559906@qq.com>
2019-06-02 10:55:04 +0800

    保证学生的唯一性

--- rfTest/src/Student01.java
+++ rfTest/src/Student01.java
@@ -20,10 +20,20 @@ public class Student01 implements Comparable<Student01>{
 	@Override
 	public int compareTo(Student01 o) {
 		// TODO Auto-generated method stub
-		 
-		return 0;
+		
+			   Student01 stu = (Student01) o;
+				int  result = compare(this.name,stu.name)?1:(age>stu.age?0:-1);
+				if(0==result) {
+					result=name.compareTo(stu.name);
+				}
+				return result;			
 	}
-
+	
+	private boolean compare(String name2, String name3) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
