66264def3cfcad538584cbcd00c410b8f056ba8e
AAASus <1803559906@qq.com>
2019-06-17 22:23:10 +0800

    对学生对象按综合测评总分升序排列

--- rfTest/src/Student02.java
+++ rfTest/src/Student02.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Student02 implements Comparable<Student02> {
 	private int id;
@@ -18,11 +17,40 @@ public class Student02 implements Comparable<Student02> {
 		return "id=" + id + ", name=" + name + ", grade=" + grade;
 	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getGrade() {
+		return grade;
+	}
+
+	public void setGrade(double grade) {
+		this.grade = grade;
+	}
+
 	@Override
 	public int compareTo(Student02 o) {
 		// TODO Auto-generated method stub
-		return 0;
+		if(this.getGrade()>o.getGrade()) {
+			return 1;
+		}
+		return -1;
 	}
+
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -31,8 +59,16 @@ public class Student02 implements Comparable<Student02> {
 		list.add(new Student02(02,"b",90.50));
 		list.add(new Student02(03,"c",88.88));
 		list.add(new Student02(01,"d",75.00));
+		Collections.sort(list);
+		Iterator<Student02> it = list.iterator();
+		while(it.hasNext()) {
+			Student02 s = it.next();
+			System.out.println(s);
+		}
 
 	}
+
+
 
 	
 }
